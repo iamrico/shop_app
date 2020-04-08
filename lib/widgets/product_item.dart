ae33b5fcd0ad48a92d7d3a93dfc72604f40e95d4
Rico Mesina <rico@sym.ph>
2020-04-08 14:24:42 +0800

    initial commit

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import '../screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
+import '../providers/product.dart';
+
+class ProductItem extends StatelessWidget {
+  ProductItem();
+
+  @override
+  Widget build(BuildContext context) {
+    //final product = Provider.of<Product>(context); alternative syntax, side note: every time this is called, it runs entire build method again, having consumer is better cuz u can insert precisely where data should change so fewer widgets rebuild
+    //Consumer.of() is used for better widget splitting and rebuilding
+	
+    return Consumer(
+		builder: (ctx, product, child) => ClipRRect(
+        	borderRadius: BorderRadius.circular(10),
+        	child: GridTile(
+				child: GestureDetector(
+					onTap: () {
+						Navigator.of(context).pushNamed(
+							ProductDetailScreen.routeName,
+							arguments: product.id,
+						);
+					},
+					child: Image.network(
+						product.imageUrl,
+						fit: BoxFit.cover,
+					),
+				),
+				footer: GridTileBar(
+						leading: IconButton(
+						icon: Icon(
+							product.isFavorite ? Icons.favorite : Icons.favorite_border),
+						onPressed: () {
+							product.toggleFavorite();
+						},
+						color: Theme.of(context).accentColor,
+						),
+						trailing: IconButton(
+							icon: Icon(Icons.shopping_cart),
+							onPressed: () {},
+							color: Theme.of(context).accentColor,
+						),
+						backgroundColor: Colors.black54,
+						title: Text(
+						product.title,
+						textAlign: TextAlign.center,
+						),
+				),
+			),
+    	),
+	);
+  }
+}
