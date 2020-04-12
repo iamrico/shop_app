0bd122e24f2718732c56c8cb7f7a57310be461b7
Rico Mesina <rico@sym.ph>
2020-04-12 17:34:42 +0800

    finished adding undo snackbar

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -29,10 +29,10 @@ class Cart with ChangeNotifier {
     var total = 0.0;
 
     _items.forEach((key, cartItem) {
-      total +=  cartItem.price * cartItem.quantity;
+      total += cartItem.price * cartItem.quantity;
     });
 
-    return total; 
+    return total;
   }
 
   void addItem(String productId, double price, String title) {
@@ -69,5 +69,24 @@ class Cart with ChangeNotifier {
     _items = {};
     notifyListeners();
   }
-  
+
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+
+    if (_items[productId].quantity > 1) {
+      _items.update(
+          productId,
+          (existingCartItem) => CartItem(
+              id: existingCartItem.id,
+              title: existingCartItem.title,
+              price: existingCartItem.price,
+              quantity: existingCartItem.quantity - 1));
+    } else {
+      _items.remove(productId);
+    }
+
+    notifyListeners();
+  }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -4,7 +4,6 @@ import 'package:provider/provider.dart';
 import '../providers/product.dart';
 import '../providers/cart_provider.dart';
 
-
 class ProductItem extends StatelessWidget {
   ProductItem();
 
@@ -12,49 +11,59 @@ class ProductItem extends StatelessWidget {
   Widget build(BuildContext context) {
     //final product = Provider.of<Product>(context); alternative syntax, side note: every time this is called, it runs entire build method again, having consumer is better cuz u can insert precisely where data should change so fewer widgets rebuild
     //Consumer is used for better widget splitting and rebuilding
-	
+
     final cart = Provider.of<Cart>(context, listen: false);
 
     return Consumer<Product>(
-		builder: (ctx, product, child) => ClipRRect(
-        	borderRadius: BorderRadius.circular(10),
-        	child: GridTile(
-				child: GestureDetector(
-					onTap: () {
-						Navigator.of(context).pushNamed(
-							ProductDetailScreen.routeName,
-							arguments: product.id,
-						);
-					},
-					child: Image.network(
-						product.imageUrl,
-						fit: BoxFit.cover,
-					),
-				),
-				footer: GridTileBar(
-						leading: IconButton(
-						icon: Icon(
-							product.isFavorite ? Icons.favorite : Icons.favorite_border),
-						onPressed: () {
-							product.toggleFavorite();
-						},
-						color: Theme.of(context).accentColor,
-						),
-						trailing: IconButton(
-							icon: Icon(Icons.shopping_cart),
-							onPressed: () {
-                cart.addItem(product.id, product.price, product.title);
+      builder: (ctx, product, child) => ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed(
+                ProductDetailScreen.routeName,
+                arguments: product.id,
+              );
+            },
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          footer: GridTileBar(
+            leading: IconButton(
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              onPressed: () {
+                product.toggleFavorite();
               },
-							color: Theme.of(context).accentColor,
-						),
-						backgroundColor: Colors.black54,
-						title: Text(
-						product.title,
-						textAlign: TextAlign.center,
-						),
-				),
-			),
-    	),
-	);
+              color: Theme.of(context).accentColor,
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {
+                cart.addItem(product.id, product.price, product.title);
+                Scaffold.of(context).hideCurrentSnackBar();
+                Scaffold.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Added Item to Cart'),
+                    duration: Duration(seconds: 2),
+                    action: SnackBarAction(label: 'UNDO', onPressed: (){
+                      cart.removeSingleItem(product.id);
+                    }),
+                  ),
+                );
+              },
+              color: Theme.of(context).accentColor,
+            ),
+            backgroundColor: Colors.black54,
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
